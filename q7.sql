48de75bc21a7f3b610c98ec18d21d63a3778f5b2
keertiG-1296 <keerti.s.g2020@gmail.com>
2026-02-06 14:06:26 +0530

    new commit

---
+++ q7.sql
@@ -0,0 +1,14 @@
+Show departments with more than 2 employees who have a salary NOT NULL, and display the number of such employees.
+
+emp_id	name	salary	department
+    1	Ravi	50000	IT
+    2	Asha	NULL	HR
+    3	John	55000	IT
+    4	Meena	NULL	HR
+    5	Ali	   70000	IT
+    6	Sara	65000	Sales
+    7	Tina	62000	HR
+
+
+SELECT name , salary, department 
+FROM employee
